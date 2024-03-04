b5c33f938fa9181b480cd1cd11932e69ef7865ef
lakshaycode22 <lakshaycode22@gmail.com>
2024-03-04 18:04:49 +0530

    courses and about us

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { SectionWrapper } from "../hoc";
 import { motion } from "framer-motion";
+import { FaArrowCircleRight } from "react-icons/fa";
 import { textVariant } from "../utils/motion";
 
 const AboutUs = () => {
   return (
     <div className="bg-primary-900">
       <div className="mx-8 max-w-6xl lg:mx-auto pb-16 mt-8">
-        <motion.h1 variants={textVariant()} className="md:text-8xl text-5xl font-black text-center md:text-left mb-4 md:mb-8 text-darkGreen">
+        <motion.h1
+          variants={textVariant()}
+          className="md:text-8xl text-5xl font-black text-center md:text-left mb-4 md:mb-8 text-darkGreen"
+        >
           Welcome to Conceptual Learning
         </motion.h1>
 
-        <p className="md:text-3xl text-xl text-justify md:text-left mb-4 md:mb-8">
+        <p className="md:text-3xl text-xl text-justify md:text-left mb-4 md:mb-8 font-semibold">
           At Conceptual Learning , we believe that every student has the
           potential to excel academically and grow into confident, well-rounded
           individuals. We extend a warm welcome to you as you embark on this
           transformative journey with us.
         </p>
 
-        <motion.h1 variants={textVariant(1)} className="md:text-5xl text-3xl font-black text-center text-darkBlue md:text-left text- mb-4 md:mb-8">
+        <motion.h1
+          variants={textVariant(1)}
+          className="md:text-5xl text-3xl font-black text-center text-darkGreen md:text-left text- mb-4 md:mb-8"
+        >
           About Us
         </motion.h1>
         <p className="md:text-xl text-md text-justify md:text-left mb-4 md:mb-8">
@@ -28,9 +35,19 @@ const AboutUs = () => {
           coaching programs in Mathematics, Science, English, Social Studies to
           empower students with the knowledge and skills they need to succeed.
         </p>
-        <motion.h1 variants={textVariant(1.5)} className="md:text-5xl text-3xl font-black text-center md:text-left text-darkYellow text- mb-4 md:mb-8">
+
+        <img
+          src="https://images.unsplash.com/photo-1521999693742-4717d76f97cc?q=80&w=2938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          className="mb-8 md:w-3/5 mx-auto"
+        ></img>
+
+        <motion.h1
+          variants={textVariant(1.5)}
+          className="md:text-5xl text-3xl font-black text-center md:text-left text-darkGreen text- mb-4 md:mb-8"
+        >
           Mission and Values
         </motion.h1>
+
         <p className="md:text-xl text-md text-justify md:text-left mb-4 md:mb-8">
           Our mission at Conceptual Learning is to cultivate a dynamic learning
           environment that fosters academic excellence, holistic development,
@@ -38,45 +55,68 @@ const AboutUs = () => {
         </p>
         <ul className="md:text-xl text-md text-justify md:text-left mb-4 md:mb-8">
           <li className="md:mb-4 mb-2">
-            <span className="text-darkGreen font-bold">Academic Excellence: </span>
+            <span className="text-darkGreen font-bold flex">
+              {"  "}
+              <FaArrowCircleRight className="my-auto mr-2" />
+              Academic Excellence:{" "}
+            </span>
             Providing rigorous academic programs that challenge and inspire
             students to reach their full potential.
           </li>
           <li className="md:mb-4 mb-2">
-            <span className="text-darkBlue font-bold">Holistic Development: </span>
+            <span className="text-darkGreen font-bold flex">
+              {" "}
+              <FaArrowCircleRight className="my-auto mr-2" />
+              Holistic Development:{" "}
+            </span>
             Nurturing the intellectual, emotional, and social development of
             students to prepare them for success in an ever-changing world.
           </li>
           <li className="md:mb-4 mb-2">
-            <span className="text-darkYellow font-bold">Personalized Learning: </span>
+            <span className="text-darkGreen font-bold flex">
+              <FaArrowCircleRight className="my-auto mr-2" />
+              Personalized Learning:{" "}
+            </span>
             Tailoring our approach to meet the individual needs and learning
             styles of each student, fostering a supportive and inclusive
             learning community.
           </li>
         </ul>
 
-        <p className="md:text-3xl text-xl text-justify md:text-left mb-4 md:mb-8 ">
+        <p className="md:text-3xl text-xl text-justify md:text-left mb-4 md:mb-8 font-semibold">
           At Conceptual Learning, our core values guide every aspect of our
           practices and interactions. We uphold:
         </p>
         <ul className="md:text-xl text-md text-justify md:text-left mb-4 md:mb-8">
           <li className="md:mb-4 mb-2">
-            <span className="text-darkGreen font-bold">Integrity: </span>Operating
-            with honesty, transparency, and ethical conduct in all our
+            <span className="text-darkGreen font-bold flex">
+              <FaArrowCircleRight className="my-auto mr-2" />
+              Integrity:{" "}
+            </span>
+            Operating with honesty, transparency, and ethical conduct in all our
             endeavors.
           </li>
           <li className="md:mb-4 mb-2">
-            <span className="text-darkBlue font-bold">Innovation: </span>Embracing
-            creativity, adaptability, and continuous improvement to enhance the
-            learning experience.
+            <span className="text-darkGreen font-bold flex">
+              <FaArrowCircleRight className="my-auto mr-2" />
+              Innovation:{" "}
+            </span>
+            Embracing creativity, adaptability, and continuous improvement to
+            enhance the learning experience.
           </li>
           <li className="md:mb-4 mb-2">
-            <span className="text-darkYellow font-bold">Inclusivity: </span>Celebrating
-            diversity and creating an environment where every student feels
-            valued, respected, and supported.
+            <span className="text-darkGreen font-bold flex">
+              <FaArrowCircleRight className="my-auto mr-2" />
+              Inclusivity:{" "}
+            </span>
+            Celebrating diversity and creating an environment where every
+            student feels valued, respected, and supported.
           </li>
           <li className="md:mb-4 mb-2">
-            <span className="text-darkGreen font-bold">Student-Centeredness: </span>
+            <span className="text-darkGreen font-bold flex">
+              <FaArrowCircleRight className="my-auto mr-2" />
+              Student-Centeredness:{" "}
+            </span>
             Placing students at the heart of everything we do, prioritizing
             their needs, aspirations, and well-being.
           </li>
@@ -85,7 +125,7 @@ const AboutUs = () => {
             values in our students, empowering them to become lifelong learners,
             critical thinkers, and responsible global citizens.
           </p>
-          <p className="md:text-3xl text-xl text-justify md:text-left mb-4 md:mb-8">
+          <p className="md:text-3xl text-xl text-justify md:text-left mb-4 md:mb-8 font-semibold">
             Welcome to Conceptual Learning, where every student&#39;s journey to
             success begins!
           </p>
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { MdArrowForwardIos } from "react-icons/md";
 import { courses } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { motion } from "framer-motion";
@@ -13,13 +13,14 @@ const Courses = () => {
     "Class XI(Commerce)",
     "Class XII(Commerce)",
   ];
+  const color = ["green", "blue", "yellow"];
 
   return (
     <div className="bg-primary-900">
       <div className="mx-8 max-w-6xl lg:mx-auto pb-16 text-justify">
         <motion.h1
           variants={textVariant()}
-          className="text-5xl font-extrabold text-center mb-16 text-secondary-500"
+          className="text-5xl font-extrabold text-center mb-16"
         >
           Courses
         </motion.h1>
@@ -29,20 +30,13 @@ const Courses = () => {
             {grades &&
               grades.map((grade, key) => {
                 return (
-                  <div className="green-blue-gradient p-1 rounded-lg">
-                    <motion.button
-                      key={key}
-                      variants={fadeIn(
-                        "right",
-                        "spring",
-                        0.5 * (key % 4),
-                        0.75
-                      )}
+                  <div className="rounded-lg mb-2">
+                    <button
                       onClick={() => setData(courses[key])}
-                      className=" rounded-lg w-60 p-4 bg-primary-900 hover:bg-secondary-600 hover:border-secondary-500 hover:text-primary-900 "
+                      className={`rounded-lg w-60 p-4  text-black bg-${color[key % 3]}`}
                     >
                       {grade}
-                    </motion.button>
+                    </button>
                   </div>
                 );
               })}
@@ -51,39 +45,42 @@ const Courses = () => {
 
         {data && (
           <div className="">
-            <motion.h1
+            
+            <h1
               variants={textVariant()}
               className="text-center font-semibold mt-8 text-secondary-500 text-2xl md:text-4xl"
             >
               {data.class}
-            </motion.h1>
+            </h1>
+            <div className="m-auto">
+                  <img
+                    src="https://images.unsplash.com/photo-1509062522246-3755977927d7?q=80&w=3032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    className="mt-10"
+                  />
+                </div>
             <div className="text-md md:text-lg">
-              <div className="md:flex mb-8 md:mb-0 ">
+              <div className=" mb-8 md:mb-0 ">
                 <div className="flex-1 py-8 max-w-screen-xl md:text-left lg:py-16">
                   <p className="text-justify">{data.brief}</p>
                 </div>
 
-                <div className="flex-1 flex flex-col  my-auto">
-                  <img
-                    src="https://images.pexels.com/photos/1001914/pexels-photo-1001914.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                    className="ml-auto md:w-10/12 align-center"
-                  />
-                </div>
+                {/* <div className="flex-1 flex flex-col  my-auto"> */}
+                
               </div>
 
-              <div className="md:flex md:justify-around">
+              <div className="md:flex md:gap-10 mb-10">
                 {data.subjects &&
                   data.subjects.map((subject, key) => {
                     return (
-                      <div key={key} className="text-center md:text-left">
-                        <h2 className="text-2xl font-semibold text-secondary-500 my-2">
-                          {subject.title}
-                        </h2>
-                        <ul className="list-disc">
-                          {subject.chapters.map((chapter, key) => {
-                            return <li key={key}>{chapter}</li>;
-                          })}
-                        </ul>
+                      <div
+                        key={key}
+                        className={`p-20 shadow-lg shadow-${color[key%3]}/50 hover:border-2 hover:border-${color[key%3]}`}
+                      >
+                        {
+                          subject.title.split(" ")[
+                            subject.title.split(" ").length - 1
+                          ]
+                        }
                       </div>
                     );
                   })}
@@ -98,6 +95,27 @@ const Courses = () => {
                     return <li key={key}>{attribute}</li>;
                   })}
               </ul>
+
+
+              <div className="md:flex gap-8 mt-10">
+                {data.subjects &&
+                  data.subjects.map((subject, key) => {
+                    return (
+                      <div key={key} className="text-center md:text-left border-2 mb-4 shadow-lg shadow-black/50">
+                        <h2 className="text-2xl font-semibold text-secondary-500 p-4 border-b-2">
+                          {subject.title}
+                        </h2>
+                        <ul className="m-2">
+                          {subject.chapters.map((chapter, key) => {
+                            return <li key={key} className="flex p-1 text-left"> <MdArrowForwardIos className="my-auto mx-2"/> {chapter}</li>;
+                          })}
+                        </ul>
+                      </div>
+                    );
+                  })}
+              </div>
+
+             
 
               <p className="text-tertiary-200 my-16">{data.additional_info}</p>
             </div>
